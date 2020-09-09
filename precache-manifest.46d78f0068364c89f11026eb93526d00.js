73d8e5785322559031997bb801839fbfc901419f
Amy Rogers <amymauderogers@gmail.com>
2020-09-09 21:31:05 +0100

    Updates

---
+++ precache-manifest.46d78f0068364c89f11026eb93526d00.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4a315a4ed1329b568dd1309cfe73c3b",
+    "url": "/lwp-design-task/index.html"
+  },
+  {
+    "revision": "ebbb7df1dd3031996f9a",
+    "url": "/lwp-design-task/static/css/main.9de69f34.chunk.css"
+  },
+  {
+    "revision": "0a7f6394543a54291f01",
+    "url": "/lwp-design-task/static/js/2.d1f24ec5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lwp-design-task/static/js/2.d1f24ec5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebbb7df1dd3031996f9a",
+    "url": "/lwp-design-task/static/js/main.9f679e98.chunk.js"
+  },
+  {
+    "revision": "5bfae89f144605a1e94c",
+    "url": "/lwp-design-task/static/js/runtime-main.f91b18a1.js"
+  },
+  {
+    "revision": "00bd684a902e4cb3385ee527e431c70a",
+    "url": "/lwp-design-task/static/media/Chaloops.00bd684a.otf"
+  },
+  {
+    "revision": "ecf0c937caa9bb7214049c0c2dff1811",
+    "url": "/lwp-design-task/static/media/english.ecf0c937.png"
+  },
+  {
+    "revision": "a2f9dd1a662385b5c5d3ce616a1cd64d",
+    "url": "/lwp-design-task/static/media/mathscot.a2f9dd1a.png"
+  }
+]);
